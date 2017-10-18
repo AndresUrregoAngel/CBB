0479e307e6704ed8421cef88887a6e26015c1e75
AndresUrregoAngel <ingenieroandresangel@gmail.com>
2017-10-17 21:40:59 -0400

    new
    new

--- BD6/s_practice/src/main/scala/practiqueI.scala
+++ BD6/s_practice/src/main/scala/practiqueI.scala
@@ -1,3 +1,6 @@
+import scala.collection.mutable.ListBuffer
+
+
 object practiqueI extends App{
 //exercise 1
 /*  val liste = List(15,39,22,98,37,19,5)
@@ -11,24 +14,33 @@ object practiqueI extends App{
   println("the total is:",z)*/
 
 //exercise 2
-  val list_etxt = List("Introduction à la programation ",
+  val list_txt = List("Introduction à la programmation ",
     "Practique de la programmation",
     "Structure de données",
     "principe de la programmation",
-    "Algorithme","Langages de progrmmation")
+    "Algorithme","Langages de programmation")
 
-  val word_cle: String = "programation"
-  var  counter_word:Int = 0
 
-  for(i<-list_etxt){
-    var test: String = i.toLowerCase
-    if(test == word_cle){
-      counter_word += 1
+  def wordrepeted(some: _*)   {
+    var  counter_word:Int = 0
+    var counter_nword: Int =0
+    val word_cle: String = "programmation"
+    var exist_word: Boolean = false
+
+    for (i <- some) {
+      var test: String = i.toLowerCase
+      if (test.contains(word_cle)) {
+        counter_word +=  1
+        exist_word = true
+       } else {
+         counter_nword += 1
+      }
     }
-
+    print(counter_word,counter_nword,exist_word)
   }
 
-  println(counter_word)
+ wordrepeted(list_txt)
+
 }
 
 
