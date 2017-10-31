3cde75d9184a4c775b9d83936c54e62ad6d9ded7
Andres Urrego <ingenieroandresangel@gmail.com>
2017-10-31 19:41:59 -0400

    Update spark_python.py

--- BD6/s_practice/spark_python.py
+++ BD6/s_practice/spark_python.py
@@ -1,4 +1,21 @@
 from operator import add
+
+
+fichier = sc.textFile("file:////home/ingenieroandresangel/datasets/BixiMontrealRentals2017/2017/OD_2017-09.csv")
+wc = fichier.flatMap(lambda x: x.split(',')).map(lambda x: (x,1)).reduceByKey(add)
+wc.saveAsTextFile("file:////home/ingenieroandresangel/datasets/sortiepython")
+
+
+
+########################################
+#### version script ###########
+########################################
+
+from operator import add
+from pyspark import SparkContext,SparkConf
+
+sc= SparkConf().setAppName("pyspark")
+
 fichier = sc.textFile("file:////home/ingenieroandresangel/datasets/BixiMontrealRentals2017/2017/OD_2017-09.csv")
 wc = fichier.flatMap(lambda x: x.split(',')).map(lambda x: (x,1)).reduceByKey(add)
 wc.saveAsTextFile("file:////home/ingenieroandresangel/datasets/sortiepython")
