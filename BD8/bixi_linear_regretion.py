9de33c4b07e91cd6e30ab4dfe3bde71b3c2373f5
Andres Urrego <ingenieroandresangel@gmail.com>
2018-02-14 22:42:47 -0500

    Update bixi_linear_regretion.py

--- BD8/bixi_linear_regretion.py
+++ BD8/bixi_linear_regretion.py
@@ -20,8 +20,9 @@ df_csv = pn.concat(csv)
 df_csv['start_date_dt']= pn.to_datetime(df_csv['start_date'],infer_datetime_format=True)
 df_csv['start_day'] = df_csv['start_date_dt'].dt.weekday_name
 df_csv['start_hour'] = df_csv['start_date_dt'].dt.hour
+df_csv['start_month'] = df_csv['start_date_dt'].dt.month
 df_start = df_csv.drop(df_csv.columns[[0,2,3,4,5,6]],axis=1)
-df_start_summ = df_start.groupby(['start_station_code', 'start_day','start_hour']).size().reset_index(name='start_counts')
+df_start_summ = df_start.groupby(['start_station_code', 'start_month','start_day','start_hour']).size().reset_index(name='start_counts')
 #print(df_start_summ.head())
 
 # data readiness for stations as ending
@@ -29,17 +30,18 @@ df_start_summ = df_start.groupby(['start_station_code', 'start_day','start_hour'
 df_csv['end_date_dt']= pn.to_datetime(df_csv['end_date'],infer_datetime_format=True)
 df_csv['end_day'] = df_csv['end_date_dt'].dt.weekday_name
 df_csv['end_hour'] = df_csv['end_date_dt'].dt.hour
+df_csv['end_month'] = df_csv['end_date_dt'].dt.month
 df_end = df_csv.drop(df_csv.columns[[0,1,2,4,5,6,7,8,9]],axis=1)
-df_end_summ = df_end.groupby(['end_station_code', 'end_day','end_hour']).size().reset_index(name='end_counts')
+df_end_summ = df_end.groupby(['end_station_code', 'end_month','end_day','end_hour']).size().reset_index(name='end_counts')
 #print(df_end_summ.head())
 
 df_rawdata = pn.merge(df_start_summ,df_end_summ,
-                      left_on=['start_station_code', 'start_day', 'start_hour']
-                      ,right_on=['end_station_code', 'end_day', 'end_hour']
+                      left_on=['start_station_code', 'start_month','start_day', 'start_hour']
+                      ,right_on=['end_station_code', 'end_month','end_day', 'end_hour']
                       ,how='inner')
 
-df_rawdata_cl = df_rawdata.drop(df_rawdata.columns[[4,5,6]],axis=1)
-df_rawdata_cl.columns = ['StationCode','Day','Hour','DepartureQty','ArrivalQty']
+df_rawdata_cl = df_rawdata.drop(df_rawdata.columns[[5,6,7,8]],axis=1)
+df_rawdata_cl.columns = ['StationCode','Month','Day','Hour','DepartureQty','ArrivalQty']
 df_rawdata_cl['Add/Remove'] = df_rawdata_cl['ArrivalQty'] - df_rawdata_cl['DepartureQty']
 
-df_rawdata_cl.head()
+df_rawdata_cl.head() 
